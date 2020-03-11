e4bb799d228b1ef0c41dfaac58703532dff09537
Madeline Kosse <mk@gower.st>
2020-03-11 18:40:43 +0000

    WIP: start adding table

--- src/cljs/the_offsite_rule/form.cljs
+++ src/cljs/the_offsite_rule/form.cljs
@@ -2,15 +2,18 @@
   (:require [reagent.core :as reagent :refer [atom]]
             [cljs-http.client :as http]))
 
-(defn person-row [person]
-  [:div
-   [:input {:type :text
-            :placeholder "Name"
-            :on-change (fn [e] (swap! person #(assoc % :name (-> e .-target .-value))))}]
-   [:input {:type :text
-            :placeholder "Postcode"
-            :on-change (fn [e] (swap! person #(assoc % :postcode (-> e .-target .-value))))}]
-   ])
+(defn person-row [people]
+    [:div
+     [:input {:type :text
+              :placeholder "Name"
+              :on-change (fn [e] (swap! person #(assoc % :name (-> e .-target .-value))))}]
+     [:input {:type :text
+              :placeholder "Postcode"
+              :on-change (fn [e] (swap! person #(assoc % :postcode (-> e .-target .-value))))}]
+     ])
+
+(defn add-person [people]
+  (swap! people #(conj % {})))
 
 (defn submit-people [people]
   (let [data (->> people
@@ -18,15 +21,27 @@
                   (map (fn [x] @x)))]
     (http/post "/api/save" {:form-params {:people (prn-str data)}})))
 
+(defn table-row [record]
+  [:tr
+   [:td (:name record)]
+   [:td (:postcode record)]])
+
+(defn table [people]
+  [:table
+   [:tr
+    [:th "Name"]
+    [:th "Postcode"]]
+   (for [row @people]
+     (table-row row))])
+
 (defn entry-rows [num-people]
-  (let [people (repeat num-people (atom {}))]
+  (let [people (atom [{:name "m" :postcode "m"}])]
     [:div
-    (map person-row people)
+     (table people)
      [:input {:type :button
               :value :submit
-              :on-click #(submit-people people)}
-      ]]))
-
+              :on-click #(submit-people people)}]
+     ]))
 (defn postcode-entry-form []
   [:div "form goes here"
    (entry-rows 2)])
