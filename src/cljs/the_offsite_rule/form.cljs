f371bbf41bdc5330958485554609d108d857e7cd
Madeline Kosse <mk@gower.st>
2020-03-18 14:50:12 +0000

    Change entry form to allow adding or removing rows

--- src/cljs/the_offsite_rule/form.cljs
+++ src/cljs/the_offsite_rule/form.cljs
@@ -2,46 +2,68 @@
   (:require [reagent.core :as reagent :refer [atom]]
             [cljs-http.client :as http]))
 
-(defn person-row [people]
-    [:div
-     [:input {:type :text
-              :placeholder "Name"
-              :on-change (fn [e] (swap! person #(assoc % :name (-> e .-target .-value))))}]
-     [:input {:type :text
-              :placeholder "Postcode"
-              :on-change (fn [e] (swap! person #(assoc % :postcode (-> e .-target .-value))))}]
-     ])
+(defonce people (atom []))
 
-(defn add-person [people]
-  (swap! people #(conj % {})))
-
-(defn submit-people [people]
-  (let [data (->> people
-                  (remove #(= % {}))
-                  (map (fn [x] @x)))]
-    (http/post "/api/save" {:form-params {:people (prn-str data)}})))
+(defn remove-person [person people]
+  (remove #(= % person) people))
 
 (defn table-row [record]
   [:tr
    [:td (:name record)]
-   [:td (:postcode record)]])
+   [:td (:postcode record)]
+   [:td [:input {:type :button
+                 :value "-"
+                 :on-click #(swap! people (partial remove-person record))}]]])
 
-(defn table [people]
-  [:table
-   [:tr
-    [:th "Name"]
-    [:th "Postcode"]]
-   (for [row @people]
-     (table-row row))])
+(defn valid-person? [record]
+  (let [{name :name postcode :postcode} record]
+    (and (some? name)
+         (some? postcode)
+         (not (= name ""))
+         (not (= postcode "")))))
 
-(defn entry-rows [num-people]
-  (let [people (atom [{:name "m" :postcode "m"}])]
+(defn add-person [person people]
+  (let [person @person]
+    (if (valid-person? person)
+      (conj people person)
+      people)))
+
+(defn submission-row []
+  (let [person (atom {})]
+    [:tr
+     [:td
+      [:input {:type :text
+               :id :name-input
+               :on-change (fn [e] (swap! person #(assoc % :name (-> e .-target .-value))))}]]
+     [:td
+      [:input {:type :text
+               :id :postcode-input
+               :on-change (fn [e] (swap! person #(assoc % :postcode (-> e .-target .-value))))}]]
+     [:td
+      [:input
+       {:type :button
+        :value "+"
+        :on-click (fn [e]
+                    (set! (.-value (js/document.getElementById "name-input")) "")
+                    (set! (.-value (js/document.getElementById "postcode-input")) "")
+                    (swap! people #(add-person person %)))}]]]))
+
+(defn table []
+  (let [rows @people]
+    [:table
+     [:tr
+      [:th "Name"]
+      [:th "Postcode"]]
+     (for [row (reverse rows)]
+       (table-row row))
+     (submission-row)]))
+
+(defn submit-people [people]
+  (http/post "/api/save" {:form-params {:people (prn-str @people)}}))
+
+(defn postcode-entry-form []
     [:div
-     (table people)
+     (table)
      [:input {:type :button
               :value :submit
-              :on-click #(submit-people people)}]
-     ]))
-(defn postcode-entry-form []
-  [:div "form goes here"
-   (entry-rows 2)])
+              :on-click #(submit-people people)}]])
