07159aa6518ef862fc70a381c025cc076b17a471
madelinekosse <mk@gower.st>
2020-06-20 15:11:10 +0100

    Handle unknown postcodes (#31)
    Until [this issue](https://github.com/ideal-postcodes/postcodes.io/issues/535)
    is resolved, we can't find location data for some postcodes.
    
    As a short term workaround, we now look up another postcode with the same
    prefix (eg SW1) and use the location daa fro that. It's not perfect, but better
    than failure

--- src/clj/the_offsite_rule/app/io/postcodes.clj
+++ src/clj/the_offsite_rule/app/io/postcodes.clj
@@ -6,7 +6,6 @@
 
 (def url "https://api.postcodes.io/postcodes")
 
-;;TODO: handle error response
 (defn- query [postcodes]
   (try
     (-> url
@@ -14,6 +13,39 @@
         (get "result"))
     (catch Exception e
       (throw (ex/network-exception "Failed to conneect to postcode API")))))
+
+(defn- find-nearby-postcode [postcode]
+  "For a postcode that isn't found, look up another one with the same prefix and use that"
+  (let [prefix (first (string/split postcode #" "))]
+    (try (let [alternative (-> url
+                               (str "/" prefix "/autocomplete")
+                               (http/get {})
+                               (get "result")
+                               first)]
+           {:original postcode
+            :alternative alternative})
+       (catch Exception e
+         (throw
+          (ex/network-exception "Failed to connect to postcode API"))))))
+
+(defn- search-alternative-postcode [{:keys [original alternative]}]
+  "Look up location data for alternative potcode and update te query field with original one"
+  (-> alternative
+      vector
+      query
+      first
+      (assoc "query" original)))
+
+(defn- replace-missing-postcodes [query-response-rows]
+  "For any rows with no location, find a nearby postcode and use the location data for that"
+  (let [good-rows (filter (fn[r] (some? (get r "result"))) query-response-rows)]
+    (->> query-response-rows
+         (filter (fn[r] (nil? (get r "result"))))
+         (map #(get % "query"))
+         (map find-nearby-postcode)
+         (map search-alternative-postcode)
+         (concat good-rows))))
+
 
 (defn- resmap->coords [location-result]
   (let [postcode (get location-result "query")
@@ -39,6 +71,7 @@
 (defn- postcode-lookup [postcodes]
   (-> postcodes
       query
+      replace-missing-postcodes
       response->lookup))
 
 ;;TODO: we can initialise with local URL for easier testing!
